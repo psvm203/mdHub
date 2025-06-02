ba154b6dfb165c39321c0e41fdbbde87e0bdf72b
psvm203 <psvm203@proton.me>
2025-06-02 23:13:33 +0900

    Add style to icon

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use yew::prelude::*;
 #[function_component]
 fn App() -> Html {
     let markdown_icon: Html = html! {
-        <img src="images/markdown.png" />
+        <div class={"flex justify-center"}>
+            <img class={"w-64"} src="images/markdown.png" />
+        </div>
     };
 
     let credit: Html = html! {
-        <footer role="contentinfo">
+        <footer class={"fixed bottom-0 left-1/2 transform -translate-x-1/2"}
+            role="contentinfo">
             <a href={"https://www.flaticon.com/free-icons/markdown"}
                 title="markdown icon">
                 {"Markdown icon created by Muhammad Andy - Flaticon"}
